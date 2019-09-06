b4b31ef958046b59c81cb960b5b421303eebe3ff
Pasanteweb <Pasanteweb@adm.urbe.edu>
2019-09-06 16:06:21 -0400

    agg database

---
+++ server/database/boleteria.sql
@@ -0,0 +1,429 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 06-09-2019 a las 22:04:14
+-- Versión del servidor: 10.3.16-MariaDB
+-- Versión de PHP: 7.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `boleteria`
+--
+CREATE DATABASE IF NOT EXISTS `boleteria` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `boleteria`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `areas`
+--
+
+CREATE TABLE `areas` (
+  `id_area` int(11) NOT NULL,
+  `nombre` varchar(40) NOT NULL,
+  `capacidad` int(11) NOT NULL,
+  `id_zonas` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `butaca`
+--
+
+CREATE TABLE `butaca` (
+  `idbutaca` int(11) NOT NULL,
+  `ubicacion` int(11) NOT NULL,
+  `id_reserva` int(11) NOT NULL,
+  `id_area` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `datos_comunicacion`
+--
+
+CREATE TABLE `datos_comunicacion` (
+  `id_datos_comuni` int(11) NOT NULL,
+  `telefono` int(11) NOT NULL,
+  `ciudad` varchar(45) NOT NULL,
+  `municipio` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `datos_comunicacion`
+--
+
+INSERT INTO `datos_comunicacion` (`id_datos_comuni`, `telefono`, `ciudad`, `municipio`) VALUES
+(1, 12345, 'maracaibo', 'maracaibo');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `datos_personales`
+--
+
+CREATE TABLE `datos_personales` (
+  `id_datos_personales` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `apellido` varchar(45) NOT NULL,
+  `sexo` varchar(20) NOT NULL,
+  `edad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `datos_personales`
+--
+
+INSERT INTO `datos_personales` (`id_datos_personales`, `nombre`, `apellido`, `sexo`, `edad`) VALUES
+(1, 'jose', 'perozo', 'masculino', 22);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estado`
+--
+
+CREATE TABLE `estado` (
+  `id_estado` int(11) NOT NULL,
+  `estado` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `evento`
+--
+
+CREATE TABLE `evento` (
+  `idevento` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `fecha` date DEFAULT NULL,
+  `hora` time DEFAULT NULL,
+  `id_evento` int(11) NOT NULL,
+  `idusuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `lugar`
+--
+
+CREATE TABLE `lugar` (
+  `idlugar` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `id_reserva` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `lugar_evento`
+--
+
+CREATE TABLE `lugar_evento` (
+  `id_lugar_evento` int(11) NOT NULL,
+  `id_lugar` int(11) NOT NULL,
+  `id_evento` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reservas`
+--
+
+CREATE TABLE `reservas` (
+  `id_reserva` int(11) NOT NULL,
+  `fecha_reserva` datetime DEFAULT NULL,
+  `fecha_limite` datetime DEFAULT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_estado` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_evento`
+--
+
+CREATE TABLE `tipo_evento` (
+  `id_evento` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `idusuario` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `pass` varchar(45) NOT NULL,
+  `rol` tinyint(1) NOT NULL,
+  `id_datos_comuni` int(11) NOT NULL,
+  `id_datos_personales` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`idusuario`, `email`, `pass`, `rol`, `id_datos_comuni`, `id_datos_personales`) VALUES
+(2, 'perozo64@gmail.com', '$2a$10$geQ.9qdLWUbejpkn8SceRepdoYw4SiBNM48BTD', 0, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `zonas`
+--
+
+CREATE TABLE `zonas` (
+  `id_zona` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `precio` double NOT NULL,
+  `idlugar` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `areas`
+--
+ALTER TABLE `areas`
+  ADD PRIMARY KEY (`id_area`),
+  ADD KEY `id_zonas` (`id_zonas`);
+
+--
+-- Indices de la tabla `butaca`
+--
+ALTER TABLE `butaca`
+  ADD PRIMARY KEY (`idbutaca`),
+  ADD KEY `id_area` (`id_area`),
+  ADD KEY `id_reserva` (`id_reserva`);
+
+--
+-- Indices de la tabla `datos_comunicacion`
+--
+ALTER TABLE `datos_comunicacion`
+  ADD PRIMARY KEY (`id_datos_comuni`);
+
+--
+-- Indices de la tabla `datos_personales`
+--
+ALTER TABLE `datos_personales`
+  ADD PRIMARY KEY (`id_datos_personales`);
+
+--
+-- Indices de la tabla `estado`
+--
+ALTER TABLE `estado`
+  ADD PRIMARY KEY (`id_estado`);
+
+--
+-- Indices de la tabla `evento`
+--
+ALTER TABLE `evento`
+  ADD PRIMARY KEY (`idevento`),
+  ADD KEY `id_evento` (`id_evento`),
+  ADD KEY `idusuario` (`idusuario`);
+
+--
+-- Indices de la tabla `lugar`
+--
+ALTER TABLE `lugar`
+  ADD PRIMARY KEY (`idlugar`),
+  ADD KEY `id_reserva` (`id_reserva`);
+
+--
+-- Indices de la tabla `lugar_evento`
+--
+ALTER TABLE `lugar_evento`
+  ADD PRIMARY KEY (`id_lugar_evento`),
+  ADD KEY `id_lugar` (`id_lugar`),
+  ADD KEY `id_evento` (`id_evento`);
+
+--
+-- Indices de la tabla `reservas`
+--
+ALTER TABLE `reservas`
+  ADD PRIMARY KEY (`id_reserva`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_estado` (`id_estado`);
+
+--
+-- Indices de la tabla `tipo_evento`
+--
+ALTER TABLE `tipo_evento`
+  ADD PRIMARY KEY (`id_evento`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`idusuario`),
+  ADD KEY `id_datos_comuni` (`id_datos_comuni`),
+  ADD KEY `id_datos_personales` (`id_datos_personales`);
+
+--
+-- Indices de la tabla `zonas`
+--
+ALTER TABLE `zonas`
+  ADD PRIMARY KEY (`id_zona`),
+  ADD KEY `idlugar` (`idlugar`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `areas`
+--
+ALTER TABLE `areas`
+  MODIFY `id_area` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `butaca`
+--
+ALTER TABLE `butaca`
+  MODIFY `idbutaca` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `datos_comunicacion`
+--
+ALTER TABLE `datos_comunicacion`
+  MODIFY `id_datos_comuni` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `datos_personales`
+--
+ALTER TABLE `datos_personales`
+  MODIFY `id_datos_personales` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `estado`
+--
+ALTER TABLE `estado`
+  MODIFY `id_estado` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `evento`
+--
+ALTER TABLE `evento`
+  MODIFY `idevento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `lugar`
+--
+ALTER TABLE `lugar`
+  MODIFY `idlugar` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `lugar_evento`
+--
+ALTER TABLE `lugar_evento`
+  MODIFY `id_lugar_evento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `reservas`
+--
+ALTER TABLE `reservas`
+  MODIFY `id_reserva` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_evento`
+--
+ALTER TABLE `tipo_evento`
+  MODIFY `id_evento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `zonas`
+--
+ALTER TABLE `zonas`
+  MODIFY `id_zona` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `areas`
+--
+ALTER TABLE `areas`
+  ADD CONSTRAINT `areas_ibfk_1` FOREIGN KEY (`id_zonas`) REFERENCES `zonas` (`id_zona`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `butaca`
+--
+ALTER TABLE `butaca`
+  ADD CONSTRAINT `butaca_ibfk_1` FOREIGN KEY (`id_area`) REFERENCES `areas` (`id_area`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `butaca_ibfk_2` FOREIGN KEY (`id_reserva`) REFERENCES `reservas` (`id_reserva`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `evento`
+--
+ALTER TABLE `evento`
+  ADD CONSTRAINT `evento_ibfk_1` FOREIGN KEY (`id_evento`) REFERENCES `tipo_evento` (`id_evento`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `evento_ibfk_2` FOREIGN KEY (`idusuario`) REFERENCES `usuarios` (`idusuario`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `lugar`
+--
+ALTER TABLE `lugar`
+  ADD CONSTRAINT `lugar_ibfk_1` FOREIGN KEY (`id_reserva`) REFERENCES `reservas` (`id_reserva`);
+
+--
+-- Filtros para la tabla `lugar_evento`
+--
+ALTER TABLE `lugar_evento`
+  ADD CONSTRAINT `lugar_evento_ibfk_1` FOREIGN KEY (`id_lugar`) REFERENCES `lugar` (`idlugar`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `lugar_evento_ibfk_2` FOREIGN KEY (`id_evento`) REFERENCES `evento` (`idevento`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `reservas`
+--
+ALTER TABLE `reservas`
+  ADD CONSTRAINT `reservas_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`idusuario`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `reservas_ibfk_2` FOREIGN KEY (`id_estado`) REFERENCES `estado` (`id_estado`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `id_datos_comuni` FOREIGN KEY (`id_datos_comuni`) REFERENCES `datos_comunicacion` (`id_datos_comuni`),
+  ADD CONSTRAINT `id_datos_personales` FOREIGN KEY (`id_datos_personales`) REFERENCES `datos_personales` (`id_datos_personales`);
+
+--
+-- Filtros para la tabla `zonas`
+--
+ALTER TABLE `zonas`
+  ADD CONSTRAINT `zonas_ibfk_1` FOREIGN KEY (`idlugar`) REFERENCES `lugar` (`idlugar`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
