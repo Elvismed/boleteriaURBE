54e26b2ab84e5895141219c149681ed62c6a354d
Pasanteweb <Pasanteweb@adm.urbe.edu>
2019-10-18 11:45:59 -0400

    casi ready falta recovery de clave

--- server/routes/butaca.js
+++ server/routes/butaca.js
@@ -2,7 +2,6 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcryptjs');
 const conn = require('../config/db');
 const _ = require('underscore');
 const queries = require('../utils/SQL');
